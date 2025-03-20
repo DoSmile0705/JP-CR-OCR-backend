ee401d32efbfd37a3fc182278549a6f06f078338
DoSmile0705 <inoue.takeichi@gmail.com>
2025-03-20 23:30:31 +0900

    Updated app.py so that use https and add cert.pem and key.pem

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def get_image(filepath):
         return abort(404)  # Not Found
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, ssl_context=("cert.pem", "key.pem"), debug=True)
     # app.run(debug=True)
